fabc3e96368370d2ad3cf536be62cd25f156daa3
kroad0129 <kroad0129@naver.com>
2025-06-27 14:28:17 +0900

    대학, 학과 카테고리 추가

---
+++ src/main/java/com/example/auth/domain/category/dto/CollegeResponse.java
@@ -0,0 +1,11 @@
+package com.example.auth.domain.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CollegeResponse {
+    private Long id;
+    private String name;
+}
