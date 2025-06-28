d95ac6316abc7d6a05ba53624fc59d8ae9b4cfac
dlstkdals123 <dlstkdals123@naver.com>
2025-06-29 01:21:52 +0900

    [Chore] profile 추가

--- src/main/java/com/example/auth/domain/profile/Service/ProfileService.java
+++ src/main/java/com/example/auth/domain/profile/Service/ProfileService.java
@@ -1,7 +1,11 @@
 package com.example.auth.domain.profile.Service;
 
+import com.example.auth.domain.category.CollegeRepository;
+import com.example.auth.domain.category.Department;
+import com.example.auth.domain.category.DepartmentRepository;
 import com.example.auth.domain.profile.Profile;
 import com.example.auth.domain.profile.ProfileRepository;
+import com.example.auth.domain.profile.dto.ProfileDetailResponse;
 import com.example.auth.domain.profile.dto.ProfileUpdateRequest;
 import com.example.auth.domain.profile.dto.ProfileUpdateResponse;
 import com.example.auth.domain.user.User;
@@ -39,8 +43,23 @@ public class ProfileService {
         Profile updatedProfile = profileRepository.save(profile);
 
         return new ProfileUpdateResponse(
-                updatedProfile.getBio(),
-                updatedProfile.getUrl()
+                updatedProfile.getHashtag()
+        );
+    }
+
+    public ProfileDetailResponse getProfileById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        Profile profile = profileRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        return new ProfileDetailResponse(
+                id,
+                user.getUsername(),
+                user.getDepartment().getCollege(),
+                user.getStudentNumber(),
+                profile.getHashtag(),
+                user.getCommentCount()
         );
     }
 }
