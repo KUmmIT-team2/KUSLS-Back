a95f9abd8ee663be887e1a4b23b5b1c70f54cc88
Kelsey Kwon <nykwon7777@gmail.com>
2025-06-28 20:14:31 +0900

    feat : qna 조회 & 상세 조회 API 추가

---
+++ src/main/java/com/example/auth/domain/qna/dto/QnaDetailResponse.java
@@ -0,0 +1,23 @@
+package com.example.auth.domain.qna.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class QnaDetailResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String writerNickname;
+    private String collegeName;
+    private String departmentName;
+    private LocalDateTime createdAt;
+    @JsonProperty("isAnswered")
+    private boolean isAnswered;
+}
